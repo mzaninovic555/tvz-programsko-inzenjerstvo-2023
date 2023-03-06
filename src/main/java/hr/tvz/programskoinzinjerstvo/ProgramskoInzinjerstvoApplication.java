418c4d858764abd30053355d597333b358d22907
Marko Zaninović <mzaninovic555@gmail.com>
2023-03-06 18:56:27 +0100

    Init

---
+++ src/main/java/hr/tvz/programskoinzinjerstvo/ProgramskoInzinjerstvoApplication.java
@@ -0,0 +1,13 @@
+package hr.tvz.programskoinzinjerstvo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramskoInzinjerstvoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramskoInzinjerstvoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/tvz/programskoinzinjerstvo/ProgramskoInzinjerstvoApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.tvz.programskoinzinjerstvo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramskoInzinjerstvoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
