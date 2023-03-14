2c03f2d666313dfb71db34015414ddcc8237caf7
Marko Zaninović <mzaninovic555@gmail.com>
2023-03-14 21:34:30 +0100

    Added annotations to ManufacturerRepository

--- pios-app-service/src/main/java/hr/tvz/pios/model/manufacturer/ManufacturerRepository.java
+++ pios-app-service/src/main/java/hr/tvz/pios/model/manufacturer/ManufacturerRepository.java
@@ -2,7 +2,11 @@ package hr.tvz.pios.model.manufacturer;
 
 import hr.tvz.pios.model.post.Post;
 import java.util.Optional;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
+@Mapper
+@Repository
 public interface ManufacturerRepository {
 
   Optional<Post> getById(Long id);
