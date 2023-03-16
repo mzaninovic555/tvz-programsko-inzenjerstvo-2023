3abed5bd9b96e4212fd5f4a03f5325162c5d3d8d
Dominik Visković <dominikviskovic@gmail.com>
2023-03-16 21:54:41 +0100

    doc

--- pios-app-service/src/main/java/hr/tvz/pios/modul/login/LoginResponse.java
+++ pios-app-service/src/main/java/hr/tvz/pios/modul/login/LoginResponse.java
@@ -4,5 +4,6 @@ package hr.tvz.pios.modul.login;
  * Rekord koji sadrži odgovor prilikom prijave.
  *
  * @param token JWT token.
+ * @param message Opcionalna error poruka.
  */
 public record LoginResponse(String token, String message) {}
