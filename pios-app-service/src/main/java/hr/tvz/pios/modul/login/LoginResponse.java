f6df0577a5eb724155ddd85a9cf2f8c9f041f14a
Dominik Visković <dominikviskovic@gmail.com>
2023-03-18 19:20:15 +0100

    Implemented new error messages

--- pios-app-service/src/main/java/hr/tvz/pios/modul/login/LoginResponse.java
+++ pios-app-service/src/main/java/hr/tvz/pios/modul/login/LoginResponse.java
@@ -1,9 +1,11 @@
 package hr.tvz.pios.modul.login;
 
+import hr.tvz.pios.common.Message;
+
 /**
  * Rekord koji sadrži odgovor prilikom prijave.
  *
  * @param token JWT token.
  * @param message Opcionalna error poruka.
  */
-public record LoginResponse(String token, String message) {}
+public record LoginResponse(String token, Message message) {}
