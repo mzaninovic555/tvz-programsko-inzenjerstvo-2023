aef43522b9f5a94af75a35bfa7b0190c22b802ce
Marko Zaninović <mzaninovic555@gmail.com>
2023-03-21 19:15:47 +0100

    Added active profile on test

--- pios-app-service/src/test/java/hr/tvz/pios/PIOSApplicationTests.java
+++ pios-app-service/src/test/java/hr/tvz/pios/PIOSApplicationTests.java
@@ -2,8 +2,10 @@ package hr.tvz.pios;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PIOSApplicationTests {
   @Test
   void contextLoads() {}
