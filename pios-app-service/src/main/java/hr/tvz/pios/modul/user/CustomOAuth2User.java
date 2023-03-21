81018ddbb108ea9f18da886e04f2efc1e04fc613
Marko Zaninović <mzaninovic555@gmail.com>
2023-03-21 19:20:10 +0100

    Checkstyle

--- pios-app-service/src/main/java/hr/tvz/pios/modul/user/CustomOAuth2User.java
+++ pios-app-service/src/main/java/hr/tvz/pios/modul/user/CustomOAuth2User.java
@@ -7,29 +7,30 @@ import java.util.Collection;
 import java.util.Map;
 
 public class CustomOAuth2User implements OAuth2User {
-    private OAuth2User oauth2User;
 
-    public CustomOAuth2User(OAuth2User oauth2User) {
-        this.oauth2User = oauth2User;
-    }
+  private final OAuth2User oauth2User;
 
-    @Override
-    public Map<String, Object> getAttributes() {
-        return oauth2User.getAttributes();
-    }
+  public CustomOAuth2User(OAuth2User oauth2User) {
+    this.oauth2User = oauth2User;
+  }
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
+  @Override
+  public Map<String, Object> getAttributes() {
+    return oauth2User.getAttributes();
+  }
 
-    @Override
-    public String getName() {
-        return oauth2User.getAttribute("login");
-    }
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return null;
+  }
 
-    public String getEmail() {
-        return oauth2User.<String>getAttribute("email");
-    }
+  @Override
+  public String getName() {
+    return oauth2User.getAttribute("login");
+  }
+
+  public String getEmail() {
+    return oauth2User.getAttribute("email");
+  }
 
 }
--- pios-app-service/src/main/java/hr/tvz/pios/modul/user/UserService.java
+++ pios-app-service/src/main/java/hr/tvz/pios/modul/user/UserService.java
@@ -9,21 +9,21 @@ import java.util.Optional;
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+  @Autowired
+  private UserRepository userRepository;
 
-    public void processOAuthPostLogin(String username, String email) {
-        Optional<User> existUser = userRepository.getByUsername(username);
+  public void processOAuthPostLogin(String username, String email) {
+    Optional<User> existUser = userRepository.getByUsername(username);
 
-        if (existUser.isEmpty()) {
-            User newUser = new User();
-            newUser.setUsername(username);
-            newUser.setEmail(email);
-            newUser.setRole(new Role(2L, "ROLE_USER"));
-            newUser.setIsActivated(true);
+    if (existUser.isEmpty()) {
+      User newUser = new User();
+      newUser.setUsername(username);
+      newUser.setEmail(email);
+      newUser.setRole(new Role(2L, "ROLE_USER"));
+      newUser.setIsActivated(true);
 
-            userRepository.insert(newUser);
-        }
-
+      userRepository.insert(newUser);
     }
+
+  }
 }
