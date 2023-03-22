8b98efc240c78e625028056fdf5fdc0c79fdc218
Marko Zaninović <mzaninovic555@gmail.com>
2023-03-22 22:11:05 +0100

    Bazni template i mail metoda

--- pios-app-service/src/main/java/hr/tvz/pios/mail/EmailService.java
+++ pios-app-service/src/main/java/hr/tvz/pios/mail/EmailService.java
@@ -1,6 +1,7 @@
 package hr.tvz.pios.mail;
 
 import hr.tvz.pios.config.PiosProperties;
+import hr.tvz.pios.modul.user.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,8 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
 
 /**
  * Servis za slanje mailova.
@@ -21,15 +24,31 @@ public class EmailService {
   PiosProperties piosProperties;
   @Autowired
   JavaMailSender mailSender;
+  @Autowired
+  SpringTemplateEngine templateEngine;
 
 
-  private void posaljiMail(String toAddress) {
+
+  public void generirajActivationEmail(User newUser, String activationToken) {
+    String subject = "TVZ PcPartPicker - Registracija";
+    String activationURL = piosProperties.frontendUrl() + "/" + activationToken;
+
+    Context context = new Context();
+    context.setVariable("user", newUser);
+    context.setVariable("activationURL", activationURL);
+    String html = templateEngine.process("mail/registracija", context);
+
+    //TODO napraviti email template kak spada
+    posaljiMail(newUser.getEmail(), subject, html);
+  }
+
+  private void posaljiMail(String toAddress, String subject, String html) {
     MimeMessagePreparator messagePreparator = mimeMessage -> {
       MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, true);
       messageHelper.setFrom(piosProperties.mail().from());
       messageHelper.setTo(toAddress);
-      messageHelper.setSubject("Temp");
-      messageHelper.setText("generirani thymeleaf", true);
+      messageHelper.setSubject(subject);
+      messageHelper.setText(html, true);
     };
     try {
       mailSender.send(messagePreparator);
