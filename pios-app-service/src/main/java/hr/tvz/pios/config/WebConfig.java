aa737a2975912c17fbfd3b01dcaa06c3b85cec51
Dominik Visković <87649516+dviskovic@users.noreply.github.com>
2023-03-22 23:05:27 +0100

    Release (#15)
    * Release CI

--- pios-app-service/src/main/java/hr/tvz/pios/config/WebConfig.java
+++ pios-app-service/src/main/java/hr/tvz/pios/config/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.EncodedResourceResolver;
 
@@ -16,6 +17,12 @@ public class WebConfig implements WebMvcConfigurer {
 
   public WebConfig(PiosProperties piosProperties) {
     this.piosProperties = piosProperties;
+  }
+
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addViewController("/{spring:^[\\w\\-]+}").setViewName("forward:/");
+    registry.addViewController("/**/{spring:^[\\w\\-]+}").setViewName("forward:/");
   }
 
   @Override
