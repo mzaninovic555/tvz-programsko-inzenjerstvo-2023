af07b56bd8e636fbf36e194a854c8cdf8bc73c40
Dominik Visković <dominikviskovic@gmail.com>
2023-03-25 12:51:19 +0100

    Removed message from LoginResponse

--- pios-app-service/src/main/java/hr/tvz/pios/modul/login/LoginResponse.java
+++ pios-app-service/src/main/java/hr/tvz/pios/modul/login/LoginResponse.java
@@ -1,11 +1,8 @@
 package hr.tvz.pios.modul.login;
 
-import hr.tvz.pios.common.Message;
-
 /**
  * Rekord koji sadrži odgovor prilikom prijave.
  *
  * @param token JWT token.
- * @param message Opcionalna error poruka.
  */
-public record LoginResponse(String token, Message message) {}
+public record LoginResponse(String token) {}
