4a02f6c77ac7e7a65860cddc4755579c712cfafa
Marko Zaninović <mzaninovic555@gmail.com>
2023-03-25 16:35:30 +0100

    Get all iz repozitorija

--- pios-app-service/src/main/java/hr/tvz/pios/modul/component/ComponentRepository.java
+++ pios-app-service/src/main/java/hr/tvz/pios/modul/component/ComponentRepository.java
@@ -1,6 +1,7 @@
 package hr.tvz.pios.modul.component;
 
 import hr.tvz.pios.common.Type;
+import java.util.List;
 import java.util.Optional;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Repository;
 @Mapper
 @Repository
 public interface ComponentRepository {
+
+  List<Component> getAll();
 
   Optional<Component> getById(Long id);
 
