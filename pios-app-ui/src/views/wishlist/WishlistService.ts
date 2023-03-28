545980c7ecd4d5acd5e445b4ed2ecd4a60c3f7f5
Dominik Visković <dominikviskovic@gmail.com>
2023-03-28 19:58:15 +0200

    Remove mock wishlist

--- pios-app-ui/src/views/wishlist/WishlistService.ts
+++ pios-app-ui/src/views/wishlist/WishlistService.ts
@@ -1,36 +1,8 @@
 import WishlistEntry from '~/views/wishlist/WishlistEntry';
 import api from '../../common/api';
-import Type from '../component-search/Type';
 import BasicResponse from '~/common/messages/BasicResponse';
 
 export async function getUserWishlist(): Promise<WishlistEntry[]> {
-  // TODO remove mock
-  return [{
-    addedAt: new Date(),
-    id: 1,
-    component: {
-      id: 1,
-      name: 'Intel nešt',
-      type: Type.HDD,
-      price: 5,
-      imageBase64: undefined,
-      manufacturer: {name: 'Intel', id: 1},
-      data: ''
-    }
-  },
-  {
-    addedAt: new Date(),
-    id: 1,
-    component: {
-      id: 1,
-      name: 'Intel nešt',
-      type: Type.HDD,
-      price: 5,
-      imageBase64: undefined,
-      manufacturer: {name: 'Intel', id: 1},
-      data: ''
-    }
-  }];
   const response = await api.get<WishlistEntry[]>('/v1/wishlist');
   return response.data;
 }
