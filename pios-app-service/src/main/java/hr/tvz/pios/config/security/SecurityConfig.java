512807e91afe9a449d412de86bedf2561ca690d7
tinzupanovic <tin.zupanovic@hotmail.com>
2023-04-01 20:21:19 +0200

    added failure url

--- pios-app-service/src/main/java/hr/tvz/pios/config/security/SecurityConfig.java
+++ pios-app-service/src/main/java/hr/tvz/pios/config/security/SecurityConfig.java
@@ -67,6 +67,7 @@ public class SecurityConfig {
         .and()
         .oauth2Login()
         .loginPage("/oauth2/authorization/github")
+        .failureUrl("/")
         .userInfoEndpoint()
         .userService(oauthUserService)
         .and()
