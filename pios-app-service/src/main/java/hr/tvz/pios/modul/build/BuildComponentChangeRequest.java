22e605d44a55912203a086c033da08d686522e14
Dominik Visković <dominikviskovic@gmail.com>
2023-04-02 17:04:00 +0200

    Typo fix

--- pios-app-service/src/main/java/hr/tvz/pios/modul/build/BuildComponentChangeRequest.java
+++ pios-app-service/src/main/java/hr/tvz/pios/modul/build/BuildComponentChangeRequest.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-/** Rekord za slanje promjene podataka o komponenatama u buildu. */
+/** Rekord za slanje promjene podataka o komponentama u buildu. */
 public record BuildComponentChangeRequest(
     @NotBlank String link,
     @NotNull @Min(1) Long componentId,
